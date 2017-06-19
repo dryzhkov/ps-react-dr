ba04d5ebcaa9a705ce666ca5852560d76bfa30a4
Dima Ryzhkov <dimaryz@gmail.com>
2017-06-18 20:53:25 -0700

    Documentation generator

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+/** An example of how to use this very advanced component to display a greeting */
+const ExampleHelloWord = () => {
+  return <HelloWorld message="Whoever is watching this!" />
+};
+
+export default ExampleHelloWord;
